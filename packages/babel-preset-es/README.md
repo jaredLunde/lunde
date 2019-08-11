62249adf9e89113744a572fd7129c499ac31a8a0
Jared Lunde <jared.lunde@gmail.com>
2019-08-11 10:35:30 -0600

    Adds babel preset utils

---
+++ packages/babel-preset-es/README.md
@@ -0,0 +1,83 @@
+# @lunde/babel-preset-es
+A babel preset for configuring ECMAScript features to apps and packages
+
+## Installation
+`yarn add --dev @lunde/babel-preset-es`
+
+## Usage
+### `.babelrc`
+```json
+{
+  "presets": [
+    [
+      "@lunde/es", {
+        "env": {
+          "modules": false
+        }
+      }
+    ]
+  ]
+}
+```
+## Included packages
+- `@babel/cli`
+- `@babel/core`
+
+## Included presets
+- `@babel/preset-env`
+- `@babel/preset-typescript`
+
+## Included plugins
+- `@babel/plugin-transform-runtime`
+- `@babel/plugin-proposal-class-properties`
+- `@babel/plugin-proposal-export-default-from`
+- `@babel/plugin-proposal-export-namespace-from`
+- `@babel/plugin-proposal-logical-assignment-operators`
+- `@babel/plugin-proposal-nullish-coalescing-operator`
+- `@babel/plugin-transform-object-assign`
+- `@babel/plugin-proposal-object-rest-spread`
+- `@babel/plugin-proposal-optional-chaining`
+- `@babel/plugin-syntax-dynamic-import`
+- `@babel/plugin-syntax-import-meta`
+- `babel-plugin-closure-elimination`
+- `babel-plugin-macros`
+- `babel-plugin-dev-expression`
+
+## Options
+### `env`
+Defaults
+```json
+{
+  "loose": true, 
+  "useBuiltIns": false, 
+  "ignoreBrowserslistConfig": true,
+  "exclude": ["transform-typeof-symbol"]
+}
+```
+- Define `"env": false` to turn off `@babel/env`
+
+### `typescript`
+- Define `"typescript": false` to turn off `@babel/typescript`
+
+
+### `runtime`
+- *default* `false`
+- Define `"runtime": {...}` to turn on `@babel/transform-runtime`
+
+### `classProps`
+- *default* `{loose: true}`
+
+### `macros`
+- Define `"macros": false` to turn off `@babel/macros`
+
+### `objectAssign`
+- Define `"objectAssign": false` to turn off `@babel/transform-object-assign`
+
+### `restSpread`
+- Define `"restSpread": false` to turn off `@babel/proposal-rest-spread`
+
+### `closureElimination`
+- Define `"closureElimination": false` to turn off `babel-plugin-closure-elimination`
+
+### `devExpression`
+- Define `"devExpression": false` to turn off `babel-plugin-dev-expression`
---
+++ packages/babel-preset-es/index.js
@@ -0,0 +1,148 @@
+// v1.0.1 // 8/11/2019 //
+
+function req(plugin) {
+  var module = require(plugin)
+  return module.default || module
+}
+
+function isPlainObject(o) {
+  if (o !== null && typeof o === 'object') {
+    var proto = Object.getPrototypeOf(o)
+    return proto === Object.prototype || proto === null
+  }
+
+  return false
+}
+
+function deepAssign() {
+  var head = arguments[0]
+  var objects = Array.prototype.slice.call(arguments, 1)
+
+  for (var i = 0; i < objects.length; i++) {
+    var next = objects[i]
+
+    for (var key in next) {
+      var nextObj = next[key]
+
+      if (isPlainObject(nextObj) && isPlainObject(head[key])) {
+        head[key] = deepAssign(head[key], nextObj)
+      } else {
+        head[key] = nextObj
+      }
+    }
+  }
+
+  return head
+}
+
+module.exports = function(api, opt) {
+  var env = process.env.NODE_ENV
+
+  if (env === 'production') {
+    return {
+      presets: [
+        opt.env === false
+          ? {}
+          : [
+              req('@babel/preset-env'),
+              Object.assign(
+                {
+                  loose: true,
+                  useBuiltIns: false,
+                  ignoreBrowserslistConfig: true,
+                  exclude: ['transform-typeof-symbol'],
+                },
+                opt.env,
+              ),
+            ],
+        opt.typescript === false ? {} : [req('@babel/preset-typescript'), {}],
+      ],
+
+      plugins: [
+        opt.runtime === true
+          ? [
+              req('@babel/plugin-transform-runtime'),
+              Object.assign({}, opt.runtime),
+            ]
+          : {},
+        [
+          req('@babel/plugin-proposal-class-properties'),
+          Object.assign({loose: true}, opt.classProps),
+        ],
+        [req('@babel/plugin-proposal-export-default-from'), {}],
+        [req('@babel/plugin-proposal-export-namespace-from'), {}],
+        [req('@babel/plugin-proposal-logical-assignment-operators'), {}],
+        [req('@babel/plugin-proposal-nullish-coalescing-operator'), {}],
+        opt.objectAssign === false
+          ? {}
+          : [req('@babel/plugin-transform-object-assign'), {}],
+        opt.restSpread === false
+          ? {}
+          : [req('@babel/plugin-proposal-object-rest-spread'), {}],
+        [req('@babel/plugin-proposal-optional-chaining'), {}],
+        [req('@babel/plugin-syntax-dynamic-import'), {}],
+        [req('@babel/plugin-syntax-import-meta'), {}],
+        opt.closureElimination === false
+          ? {}
+          : [req('babel-plugin-closure-elimination'), {}],
+        opt.macros === false ? {} : [req('babel-plugin-macros'), {}],
+        opt.devExpression === false
+          ? {}
+          : [req('babel-plugin-dev-expression'), {}],
+      ],
+    }
+  } else {
+    return {
+      presets: [
+        opt.env === false
+          ? {}
+          : [
+              req('@babel/preset-env'),
+              Object.assign(
+                {
+                  loose: true,
+                  useBuiltIns: false,
+                  ignoreBrowserslistConfig: true,
+                  exclude: ['transform-typeof-symbol'],
+                },
+                opt.env,
+              ),
+            ],
+        opt.typescript === false ? {} : [req('@babel/preset-typescript'), {}],
+      ],
+
+      plugins: [
+        opt.runtime === true
+          ? [
+              req('@babel/plugin-transform-runtime'),
+              Object.assign({}, opt.runtime),
+            ]
+          : {},
+        [
+          req('@babel/plugin-proposal-class-properties'),
+          Object.assign({loose: true}, opt.classProps),
+        ],
+        [req('@babel/plugin-proposal-export-default-from'), {}],
+        [req('@babel/plugin-proposal-export-namespace-from'), {}],
+        [req('@babel/plugin-proposal-logical-assignment-operators'), {}],
+        [req('@babel/plugin-proposal-nullish-coalescing-operator'), {}],
+        opt.objectAssign === false
+          ? {}
+          : [req('@babel/plugin-transform-object-assign'), {}],
+        opt.restSpread === false
+          ? {}
+          : [req('@babel/plugin-proposal-object-rest-spread'), {}],
+        [req('@babel/plugin-proposal-optional-chaining'), {}],
+        [req('@babel/plugin-syntax-dynamic-import'), {}],
+        [req('@babel/plugin-syntax-import-meta'), {}],
+        opt.closureElimination === false
+          ? {}
+          : [req('babel-plugin-closure-elimination'), {}],
+        opt.macros === false ? {} : [req('babel-plugin-macros'), {}],
+        opt.devExpression === false
+          ? {}
+          : [req('babel-plugin-dev-expression'), {}],
+      ],
+    }
+  }
+}
