e21b73a978be185d113e7967670e67c385c1d803
Jared Lunde <jared.lunde@gmail.com>
2020-01-25 21:25:11 -0700

    [create-preact-app] Fixes children type

--- packages/create-preact-app/lib/shared/src/global.d.ts
+++ packages/create-preact-app/lib/shared/src/global.d.ts
@@ -3,7 +3,9 @@ declare const __CLIENT__: boolean
 declare const __DEV__: boolean
 // React-like shims
 declare type Props<P> = {[Name in keyof P]: P[Name]} & {
-  children?: JSX.Element | JSX.Element[] | string | false | number | null
+  children?:
+    | (JSX.Element | string[] | string | false | number | null)
+    | (JSX.Element | string[] | string | false | number | null)[]
 }
 declare type FC<P = {}> = {
   (props: Props<P>): JSX.Element | null
