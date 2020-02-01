bcfdc7e8107c186dc77c2b8b38b60e877987b3df
Jared Lunde <jared.lunde@gmail.com>
2020-01-31 22:57:06 -0700

    Fixes components import in create-preact-app

--- packages/create-preact-app/lib/shared/src/components/index.tsx
+++ packages/create-preact-app/lib/shared/src/components/index.tsx
@@ -1,6 +1,6 @@
 export {DesignSystem, useDesignSystem} from './DesignSystem'
 
-export {button, buttonOutline, Button} from './Button'
+export {button, buttonOutline, Button, ButtonLink} from './Button'
 export {icon, Icon} from './Icon'
 export {link, Link} from './Link'
 export {text, Text} from './Text'
