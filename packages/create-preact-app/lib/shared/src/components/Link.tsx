e57f4e36482581e84e4d55e672e46c1c8dca81ee
Jared Lunde <jared@getadmiral.com>
2020-05-18 09:41:27 -0600

    [create-preact-app] fixes Link preloading

--- packages/create-preact-app/lib/shared/src/components/Link.tsx
+++ packages/create-preact-app/lib/shared/src/components/Link.tsx
@@ -1,14 +1,19 @@
 import {h} from 'preact'
 import {Link as WouterLink} from 'wouter'
 import type {LinkProps as WouterLinkProps} from 'wouter'
+import type {AsyncComponentGetter} from 'create-async-component'
 
 export const Link: React.FC<LinkProps> = ({preload, ...props}) => (
   <WouterLink
-    onTouchStart={preload.load()}
-    onMouseEnter={preload.load()}
-    onMouseDown={preload.load()}
+    onTouchStart={preload.load}
+    onMouseEnter={preload.load}
+    onMouseDown={preload.load}
     {...props}
   />
 )
 
-export type LinkProps = WouterLinkProps & {}
+export type LinkProps = WouterLinkProps & {
+  preload: React.FC<any> & {
+    load: AsyncComponentGetter<any>
+  }
+}
